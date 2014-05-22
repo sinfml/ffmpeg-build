f7033835b4cb3b25bce1a61bdd351250d4bc9183
DAI Dan <hzdaidang@corp.netease.com>
2014-05-22 16:59:39 +0800

    init

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 ffmpeg-build
 ============
 
-Build static FFmpeg
+### Install dependencies
+```bash
+sudo apt-get install zlib1g-dev libbz2-dev libghc-bzlib-dev libmp3lame-dev libvorbis-dev libtheora-dev libxvidcore-dev libx264-dev libvpx-dev libspeex-dev libexpat-dev libxml2-dev libvo-aacenc-dev libvo-amrwbenc-dev libopencore-amrnb-dev libopencore-amrwb-dev yasm gcc
+```
+
+### Download ffmpeg source
+```bash
+wget http://www.ffmpeg.org/releases/ffmpeg-2.2.2.tar.gz
+```
+
+### Compile ffmpeg
+```
+tar zxf ffmpeg-2.2.2.tar.gz
+cp build.sh ffmpeg-2.2.2/
+cd ffmpeg-2.1.4/
+./build.sh
+```
---
+++ build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+make clean
+
+./configure \
+    --extra-cflags='-I/usr/include -static' \
+    --extra-ldflags='-I/usr/lib -static' \
+    --disable-debug \
+    --disable-shared \
+    --enable-static \
+    --enable-gpl \
+    --enable-libmp3lame \
+    --enable-nonfree \
+    --disable-logging \
+    --disable-avdevice \
+    --disable-swscale \
+    --disable-postproc \
+    --disable-dxva2 \
+    --disable-vaapi \
+    --disable-vda \
+    --disable-vdpau \
+    --disable-everything \
+    --disable-runtime-cpudetect \
+    --disable-swscale-alpha \
+    --disable-ffplay \
+    --disable-ffprobe \
+    --disable-ffserver \
+    --disable-doc \
+    --disable-htmlpages \
+    --disable-manpages \
+    --disable-podpages \
+    --disable-txtpages \
+    --enable-protocol=file \
+    --enable-protocol=pipe \
+    --enable-protocol=http \
+    --enable-protocol=https \
+    --enable-filter=aresample \
+    --enable-decoder=aac \
+    --enable-decoder=mp3 \
+    --enable-demuxer=aac \
+    --enable-demuxer=mp3 \
+    --enable-parser=aac \
+    --enable-parser=mpegaudio \
+    --enable-muxer=mp3 \
+    --enable-encoder=libmp3lame \
+    --enable-small
+
+make -j 8
+
+./ffmpeg -v 0 -formats
+
+ls -lh ffmpeg
+
